e004620a4658fd136b4b3cff8e6a2fbb47c8ca50
liaosuwei <liaosuwei@hanglorygroup.com>
2021-09-04 16:09:38 +0800

    第一个版本V1

---
+++ 1.py
@@ -0,0 +1,60 @@
+
+#print('please enter your name:')
+#name = input()
+#print('hello',name)
+#print('中文测试正常')
+'''age = 7
+if  age >=18:
+    print('your age is',age)
+    print('adult')
+elif 18 > age >= 6:
+    print('your age is',age)
+    print('teenager')
+else:
+     print('your age is',age)
+     print('kid')
+
+height = 1.75
+weight = 80.5
+bmi = weight/(height*height)
+print(bmi)
+if bmi < 18.5:
+    print('过轻')
+elif 18.5<= bmi <=25:
+    print('正常')
+elif 25 < bmi <= 28:
+   print('过重')
+elif 28 < bmi <= 32:
+   print('肥胖')
+else:
+    print('严重肥胖')'''
+'''names = ['Bob','july','candy']
+for name in names:
+    print(name)'''
+import math 
+'''def quadratic (a,b,c):
+    x1 = (-b + math.sqrt(b*b - 4*a*c)/(2*a))
+    x2 = (-b - math.sqrt(b*b - 4*a*c)/(2*a))
+    x = [x1,x2]
+    print(x)
+print(quadratic(2,4,1))
+print('请输入a,b,c的值：')
+a1 = input()
+b2 = input()
+c3 = input()
+d = [a1,b2,c3]
+print('a,b,c的值为',d)
+print('所求得的解为：',quadratic(a1,b2,c3))'''
+#name = ['liujingming','liaosuwei','zhaoxinyi','chenyahan']
+#print(name[0:4])
+print('请输入不超过五个字符的词汇：')
+'''l = ['h','e','l','l','o']
+a = input()
+alist = a.split(',')
+print(alist)
+n = -1
+for b in alist:
+    n = n + 1
+    if b != l[n]:
+        print('测试失败')
+    print('测试成功')'''
